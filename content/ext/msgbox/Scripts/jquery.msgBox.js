670652571d844e8a8b0502375aaba46caaac1ef7
brzezinavaclav <brzezina.vaclav@outlook.cz>
2016-02-27 16:46:58 +0100

    new

--- content/ext/msgbox/Scripts/jquery.msgBox.js
+++ content/ext/msgbox/Scripts/jquery.msgBox.js
@@ -100,7 +100,7 @@ function msg (options) {
     });
 
     var divBackGround = "<div id=" + divMsgBoxBackGroundId + " class=\"msgBoxBackGround\"></div>";
-    var divTitle = "<div id='msgBox1456587845594Buttons'><a class='fa fa-close msgClose' type='button' value='Close'></a></div> <div class=\"msgBoxTitle\">" + options.title + "</div>";
+    var divTitle = "<a class='fa fa-close msgClose' type='button' name='Close'></a> <div class=\"msgBoxTitle\">" + options.title + "</div>";
     var divContainer = "<div class=\"msgBoxContainer\"><div id=" + divMsgBoxContentId + " class=\"msgBoxContent\"><p><span>" + options.content + "</span></p></div></div>";
     var divButtons = "<div id=" + divMsgBoxButtonsId + " class=\"msgBoxButtons\">" + buttons + "</div>";
     var divInputs = "<div class=\"msgBoxInputs\">" + inputs + "</div>";
