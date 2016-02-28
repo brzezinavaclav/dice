e7182a6034fb8ceb01079547192c58165e89a29d
brzezinavaclav <brzezina.vaclav@outlook.cz>
2016-02-28 14:46:38 +0100

    new

--- themes/DiceCoin2/frontpage.php
+++ themes/DiceCoin2/frontpage.php
@@ -144,8 +144,8 @@
                                     <div class="label">Login:</div>
                                 </div>
                                 <div class="col-xs-8">
-                                    <span class="blue"><a href="#" id="login">Player_1</a></span>
-                                    <a href="#" onclick="javascript:return account();" id="change_login" title="Change login"><i class="fa fa-pencil"></i></a>
+                                    <span class="blue"><a href="#" id="login"><?php echo $player['alias']; ?></a></span>
+                                    <a onclick="javascript:return account();" id="change_login" title="Change login"><i class="fa fa-pencil"></i></a>
                                 </div>
                             </div>
                             <div class="row">
