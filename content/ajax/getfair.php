8bccdc008d4c237028792d8ff423a37f4d4dbe1d
brzezinavaclav <brzezina.vaclav@outlook.cz>
2016-02-28 19:08:40 +0100

    new

--- content/ajax/getfair.php
+++ content/ajax/getfair.php
@@ -21,7 +21,7 @@ $player['last_server_seed']=(double)substr($player['last_server_seed'],27);
 $player['server_seed']=(double)substr($player['server_seed'],27);
 $c=($player['last_server_seed']==0)?"<small><i>You haven't bet yet</i></small>":"<small>Server Seed sha256:</small><br><br><small><small>".hash('sha256',$player['last_seedhash'].sprintf("%.32f",$player['last_server_seed']))."</small></small><br><br><small>Server Seed:</small><br><br><small><small>".$player['last_seedhash'].sprintf("%.32f",$player['last_server_seed'])."</small></small>";
 $return=array(
-  'con' => "<br><b>Next Bet:</b><br><br><small>Server Seed sha256:</small><br><br><small><small>".hash('sha256',$player['seedhash'].sprintf("%.32f",$player['server_seed']))."</small></small><br><br><br><b>Last Bet:</b><br><br>".$c."<br><br><small><b>Note:</b> Decimals from 3rd place are cutted off.</small>"
+  'con' => "<p><b>Next Bet:</b></p>><br><small>Server Seed sha256:</small><br><br><small><small>".hash('sha256',$player['seedhash'].sprintf("%.32f",$player['server_seed']))."</small></small><br><br><p><b>Last Bet:</b></p>><br>".$c."<br><br><small><b>Note:</b> Decimals from 3rd place are cutted off.</small>"
 );
 echo json_encode($return);
 
